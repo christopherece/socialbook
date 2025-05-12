9bde09a60bbe0d7750c58847fca464a6d75df454
christopherece <christopheranchetaece@gmail.com>
2025-05-12 16:42:56 +1200

    Added nav

--- socialbook/settings.py
+++ socialbook/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
